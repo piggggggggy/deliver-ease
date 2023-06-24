7a7d6bbdba4223a74d0945415f7a8a804df78a34
TAESUNG LEE <ts.lee@vtpl.kr>
2023-06-24 15:38:50 +0900

    feat: lts - setting - init directory setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { ReviewModule } from './review/review.module';
+import { PaymentModule } from './payment/payment.module';
+import { StoreModule } from './store/store.module';
+import { CartModule } from './cart/cart.module';
+import { DishModule } from './dish/dish.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, OrderModule, ReviewModule, PaymentModule, StoreModule, CartModule, DishModule],
   controllers: [AppController],
   providers: [AppService],
 })
