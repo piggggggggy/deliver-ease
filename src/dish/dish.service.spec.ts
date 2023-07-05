3f13a1b5e5eaefe64e709c9a87ca2f600dbac163
Yongtae Park <samuel.park@megazone.com>
2023-07-05 18:08:43 +0900

    feat: sml - dish - create dish service unit & test

--- src/dish/dish.service.spec.ts
+++ src/dish/dish.service.spec.ts
@@ -12,6 +12,153 @@ describe('DishService', () => {
     service = module.get<DishService>(DishService);
   });
 
+  const argsToBeExpectedInvalid = {
+    name: 'long long long long and invalid store name',
+    stock: 'invalid stock',
+    price: 'invalid price',
+    img_url: 1234,
+    category: 'invalid category',
+    description: 'invalid description',
+    options: [
+      {
+        name: 'invalid option name',
+        price: 'invalid option price',
+      },
+      {
+        name: 'invalid option name',
+        price: -1,
+      },
+    ],
+  };
+
+  const argsToBeExpectedToReturnTrue = {
+    name: 'validName',
+    stock: 100,
+    price: 10000,
+    img_url: 'http://valid-url.com/image.jpg',
+    category: 'valid category',
+    description: 'valid description',
+    options: [
+      {
+        name: 'valid option name',
+        price: 1000,
+      },
+    ],
+  };
+
+  describe('Helper unit test function', () => {
+    describe('checkDishInfoValidation', () => {
+      it('should return false with invalid argument', () => {
+        const result = service.checkDishInfoValidation(argsToBeExpectedInvalid);
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkDishInfoValidation(
+          argsToBeExpectedToReturnTrue,
+        );
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkNameValidation', () => {
+      it('should return false', () => {
+        const result = service.checkNameValidation(
+          argsToBeExpectedInvalid.name,
+        );
+        expect(result).toBe(false);
+      });
+      it('should return true', () => {
+        const result = service.checkNameValidation('validName');
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkNumberValidation', () => {
+      it('should return false', () => {
+        const result = service.checkNumberValidation(
+          argsToBeExpectedInvalid.stock as number,
+        );
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkNumberValidation(100);
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkDishImgUrl', () => {
+      it('should return false', () => {
+        const result = service.checkDishImgUrl(
+          argsToBeExpectedInvalid.img_url as string,
+        );
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkDishImgUrl(
+          argsToBeExpectedToReturnTrue.img_url,
+        );
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkDishCategory', () => {
+      it('should return false', () => {
+        const result = service.checkDishCategory(
+          argsToBeExpectedInvalid.category,
+        );
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkDishCategory(
+          argsToBeExpectedToReturnTrue.category,
+        );
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkDishDescription', () => {
+      it('should return false', () => {
+        const result = service.checkDishDescription(
+          argsToBeExpectedInvalid.description,
+        );
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkDishDescription(
+          argsToBeExpectedToReturnTrue.description,
+        );
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('checkDishOptions', () => {
+      it('should return false', () => {
+        const result = service.checkDishOptions(
+          argsToBeExpectedInvalid.options as { name: string; price: number }[],
+        );
+        expect(result).toBe(false);
+      });
+
+      it('should return true', () => {
+        const result = service.checkDishOptions(
+          argsToBeExpectedToReturnTrue.options,
+        );
+        expect(result).toBe(true);
+      });
+    });
+  });
+
+  describe('registerDish', () => {});
+
+  describe('deleteDish', () => {});
+
+  describe('updateDish', () => {});
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
--- src/dish/dish.service.ts
+++ src/dish/dish.service.ts
@@ -1,4 +1,89 @@
 import { Injectable } from '@nestjs/common';
 
+// temporary
+interface DishEntity {
+  id: string;
+  storeId: string; // ?
+  name: string;
+  stock: number;
+  price: number;
+  img_url: string;
+  description: string;
+  category: string;
+  options: {
+    name: string;
+    price: number;
+  }[];
+}
+
+interface DishDto {
+  storeId: string; // ?
+  name: string;
+  stock: number;
+  price: number;
+  img_url: string;
+  description: string;
+  category: string;
+  options: {
+    name: string;
+    price: number;
+  }[];
+}
+
 @Injectable()
-export class DishService {}
+export class DishService {
+  private dish: DishEntity[] = [];
+
+  // service
+  registerDish(dishData: any): boolean {}
+
+  // unit function
+  checkDishInfoValidation(dishData: any): boolean {
+    const { name, stock, price, img_url, description, category, options } =
+      dishData;
+    return (
+      this.checkNameValidation(name) &&
+      this.checkDishImgUrl(img_url) &&
+      this.checkNumberValidation(price) &&
+      this.checkNumberValidation(stock) &&
+      this.checkDishCategory(category) &&
+      this.checkDishDescription(description) &&
+      this.checkDishOptions(options)
+    );
+  }
+
+  checkNameValidation(name: string): boolean {
+    const maxLength = 16;
+    return typeof name === 'string' && name.length <= maxLength && name !== '';
+  }
+
+  checkNumberValidation(price: number): boolean {
+    return typeof price === 'number' && price > 0;
+  }
+
+  checkDishImgUrl(img_url: string): boolean {
+    const urlPattern =
+      /^(http(s)?:\/\/)(www\.)?([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?$/;
+    return typeof img_url === 'string' && urlPattern.test(img_url);
+  }
+
+  checkDishCategory(category: string): boolean {
+    return typeof category === 'string' && category !== '';
+  }
+
+  checkDishDescription(description: string): boolean {
+    return typeof description === 'string' && description !== '';
+  }
+
+  checkDishOptions(options: DishDto['options']): boolean {
+    options.forEach((option) => {
+      if (!this.checkNameValidation(option.name)) {
+        return false;
+      }
+      if (!this.checkNumberValidation(option.price)) {
+        return false;
+      }
+    });
+    return true;
+  }
+}
