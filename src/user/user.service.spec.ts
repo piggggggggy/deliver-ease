77851cdcb94e0d456885ea0fcd91eedadf1aebe0
misong <misong321@gmail.com>
2023-07-05 18:55:55 +0900

    feat: Its - user - define CRD favorite store

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -12,7 +12,57 @@ describe('UserService', () => {
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('addUserFavorite', () => {
+    it('should add a favorite store for the user', () => {
+      const userId = 'user1';
+      const storeId = 'store1';
+
+      service.addUserFavorite(userId, storeId);
+
+      expect(service.getUserFavorites(userId)).toContain(storeId);
+    });
+  });
+
+  describe('removeUserFavorite', () => {
+    it('should remove a favorite store for the user', () => {
+      const userId = 'user1';
+      const storeId = 'store1';
+
+      service.addUserFavorite(userId, storeId);
+      service.removeUserFavorite(userId, storeId);
+
+      expect(service.getUserFavorites(userId)).not.toContain(storeId);
+    });
+
+    it('should not throw an error when removing a non-existent favorite store', () => {
+      const userId = 'user1';
+      const storeId = 'store1';
+
+      expect(() => {
+        service.removeUserFavorite(userId, storeId);
+      }).not.toThrow();
+    });
+  });
+
+  describe('getUserFavorites', () => {
+    it('should return user favorites', () => {
+      const userId = 'user1';
+      const storeId1 = 'store1';
+      const storeId2 = 'store2';
+
+      service.addUserFavorite(userId, storeId1);
+      service.addUserFavorite(userId, storeId2);
+
+      const favorites = service.getUserFavorites(userId);
+      expect(favorites).toContain(storeId1);
+      expect(favorites).toContain(storeId2);
+    });
+
+    it('should return an empty array if user favorites do not exist', () => {
+      const userId = 'user1';
+
+      const favorites = service.getUserFavorites(userId);
+      expect(favorites).toEqual([]);
+    });
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private userFavorites: Record<string, string[]> = {};
+
+  addUserFavorite(userId: string, storeId: string): void {
+    if (!this.userFavorites[userId]) {
+      this.userFavorites[userId] = [];
+    }
+    this.userFavorites[userId].push(storeId);
+  }
+
+  removeUserFavorite(userId: string, storeId: string): void {
+    if (this.userFavorites[userId]) {
+      const index = this.userFavorites[userId].indexOf(storeId);
+      if (index !== -1) {
+        this.userFavorites[userId].splice(index, 1);
+      }
+    }
+  }
+
+  getUserFavorites(userId: string): string[] {
+    return this.userFavorites[userId] || [];
+  }
+}
