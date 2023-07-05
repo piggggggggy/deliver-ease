6ddff13a803d24cecd860be5681eca457d9d285f
TAESUNG LEE <ts.lee@vtpl.kr>
2023-07-05 18:03:33 +0900

    feat: lts - order - create service layer logic

--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -12,37 +12,40 @@ describe('OrderService', () => {
     service = module.get<OrderService>(OrderService);
   });
 
+  const OrderStatus = {
+    ORDER_REQUEST: 'order request',
+    ORDER_CONFIRMED: 'order confirmed',
+    ORDER_COMPLETED: 'order completed',
+    ORDER_CANCEL_REQUEST: 'order cancel request',
+    ORDER_CANCEL_COMPLETED: 'order cancel completed',
+    ORDER_CANCEL_FAILED: 'order cancel failed',
+    DELIVERY_PROGRESS: 'delivery progress',
+    DELIVERY_COMPLETED: 'delivery completed',
+  };
+
   const orderInfo = {
     id: 1,
     store_id: 1,
     dish_id: 1,
     user_id: 1,
-    status: { name: 'order request' },
+    status: { name: null, timestamp: 0 },
   };
-  const orderComfirmed = {
-    id: 1,
-    store_id: 1,
-    dish_id: 1,
-    user_id: 1,
-    status: { name: 'order confirmed' },
-  };
-  const orderCompleted = {
-    id: 1,
-    store_id: 1,
-    dish_id: 1,
-    user_id: 1,
-    status: { name: 'order completed' },
-  };
+
   describe('updateOrderStatus', () => {
-    it('should be return order confirmed', async () => {
-      expect(service.updateOrderStatus(orderInfo, orderComfirmed)).toBe(
-        'order confirmed',
-      );
+    it('should be update order status and return "order confirmed"', async () => {
+      expect(
+        service.updateOrderStatus(orderInfo, OrderStatus.ORDER_CONFIRMED),
+      ).toBe('order confirmed');
     });
-    it('should be return order completed', async () => {
-      expect(service.updateOrderStatus(orderInfo, orderCompleted)).toBe(
-        'order completed',
-      );
+    it('should be update order status and return "order confirmed"', async () => {
+      expect(
+        service.updateOrderStatus(orderInfo, OrderStatus.DELIVERY_PROGRESS),
+      ).toBe('delivery progress');
+    });
+    it('should be update order status and return "order confirmed"', async () => {
+      expect(
+        service.updateOrderStatus(orderInfo, OrderStatus.ORDER_COMPLETED),
+      ).toBe('order completed');
     });
   });
 
